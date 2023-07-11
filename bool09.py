42c6baed536f8a8e8f28686a20acea47fd291975
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-11 10:58:42 +0500

    Natural son

--- bool09.py
+++ bool09.py
@@ -1,5 +1,10 @@
 def main(a):
-    """
+    son=(a>=0)
+    return son
+img=main(9)
+print(img)
+
+"""
     Check the natural number. Natural numbers are numbers used in counting.
     Args:
         a: int
@@ -7,4 +12,3 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
