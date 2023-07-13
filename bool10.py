a05395f0b0cc94dfb78733ba956810190d284b1c
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-13 10:19:59 +0500

    misol

--- bool10.py
+++ bool10.py
@@ -1,10 +1,14 @@
+from math import sqrt
 def main(a):
-    """
+    son=(sqrt(a))
+    img=(son*10)%10==0
+    return img
+img=main(9)
+print(img)
+"""
     Check that the number "a" is a perfect square.
     Args:
         a: int
     Returns:
-        bool
     """
     # Write your code here
-    return 
